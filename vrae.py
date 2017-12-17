5c9bcf5b391fc2b2dba406a19498f42f17104465
Arunesh <>
2017-12-17 16:56:17 -0500

    Added logging, model saving, adaptive learning rate, nan termination

--- vrae.py
+++ vrae.py
@@ -1,36 +1,53 @@
 import numpy as np
 import os
+import sys
 import matplotlib.pyplot as plt
 import tensorflow as tf
-#from jupyterthemes import jtplot
-#from IPython.core.debugger import Tracer
-#jtplot.style()
+from jupyterthemes import jtplot
 
 
-################################
-# Params
-################################
+#CLI args [data_set_idx, z_dim, time_steps]
+#Example: python vrae  data_set_idx=0 z_dim=20 time_steps=50
+
+
 # X \in {0,1}^{batch_size, dim_x, time_steps}
-time_steps = 10 #TEST
+
+# Constant params
 x_in_dim = 88
-z_dim = 20 #TEST
-num_hidden_units = 500
-batch_size = 2#100 #TEST
-#learning_rate = 5*1e-6
+
+# Params replicated from Fabius et. al Paper
 beta_1 = 0.05
 beta_2 = 0.001
-num_epochs = 400
-starter_learning_rate = 1e-4
-decay_rate = .6
+num_epochs = 10#40000 #TEST
+learning_rate_1 = 2e-5
+learning_rate_2 = 1e-5
+num_epochs_to_diff_learn_rate = 5#16000 #TEST 
+num_epochs_to_save_model = 4#1000 #TEST
+num_hidden_units = 500
+#decay_rate = .7
 
-load_root = './MIDI_Data_PianoRolls/Nottingham/train/'
-samples = create_samples(load_root, time_steps=time_steps, verbose=False)
-num_train_samples = 10 #TEST #samples.shape[0] #TEST only pick 20 samples and overfit
+##############################
+# Hyperparams
+##############################
+data_set_idx = int(sys.argv[1].split('=')[1])
+z_dim = int(sys.argv[2].split('=')[1])
+time_steps = int(sys.argv[3].split('=')[1])
+batch_size = 100
 
 
-################################
+# Dir paths
+data_sets = ['Nottingham','JSB Chorales']
+load_root = './MIDI_Data_PianoRolls/'
+log_root =  './logs/MIDI_Data_PianoRolls'
+
+train_dir_path = os.path.join(load_root,data_sets[data_set_idx],'train')
+valid_dir_path = os.path.join(load_root,data_sets[data_set_idx],'valid')
+test_dir_path = os.path.join(load_root,data_sets[data_set_idx],'test')
+
+
+#############################
 # Helper functions
-################################
+#############################
 def clip_roll(piano_roll, time_steps=50):
     samples = []
     num_samples = int(piano_roll.shape[1] / time_steps)
@@ -41,6 +58,10 @@ def clip_roll(piano_roll, time_steps=50):
     return samples   
 
 def create_samples(load_root, time_steps=50, verbose=False):
+    if not os.path.isdir(load_root):
+        print("Invalid load_root directory.")
+        sys.exit(0)
+        
     samples = []
     for (dirpath, dirnames, filenames) in os.walk(load_root):
         for file in filenames:
@@ -53,15 +74,65 @@ def create_samples(load_root, time_steps=50, verbose=False):
 
     return np.stack(samples)
 
-def feed_dict(batch_size):
-    indeces = np.random.randint(num_train_samples, size=batch_size)
-    return {X: np.take(samples, indeces, axis=0)}
+def feed_dict(batch_size, samples_type=None):
+    if samples_type == 'train':
+        indeces = np.random.randint(num_train_samples, size=batch_size)
+        samples = train_samples
+        
+    elif samples_type == 'valid':
+        indeces = np.random.randint(num_valid_samples, size=batch_size)
+        samples = valid_samples
+        
+    elif samples_type == 'test':
+        indeces = np.random.randint(num_test_samples, size=batch_size)
+        samples = test_samples
+        
+    return np.take(samples, indeces, axis=0)
 
 
-##################################
-# Setup tf Graph
-##################################
+#############################
+# Setup Load/Save paths
+#############################
+train_samples = create_samples(train_dir_path, time_steps=time_steps, verbose=False)
+valid_samples = create_samples(valid_dir_path, time_steps=time_steps, verbose=False)
+test_samples = create_samples(test_dir_path, time_steps=time_steps, verbose=False)
 
+num_train_samples = train_samples.shape[0] 
+num_valid_samples = valid_samples.shape[0] 
+num_test_samples = test_samples.shape[0] 
+
+
+network_params =  ''.join([
+                  'time_steps={}-'.format(time_steps),          
+                  'latent_dim={}-'.format(z_dim),          
+                  'dataset={}'.format(data_sets[data_set_idx])])
+
+# Dir structure : /base_dir/network_params/run_xx/train_or_test/
+log_base_dir = os.path.join(log_root, network_params)
+
+# Check for previous runs
+if not os.path.isdir(log_base_dir):
+    os.makedirs(log_base_dir)
+
+previous_runs = os.listdir(log_base_dir)
+
+if len(previous_runs) == 0:
+    run_number = 1
+else:
+    run_number = max([int(str.split(s,'run_')[1]) for s in previous_runs if 'run' in s]) + 1
+
+log_dir = os.path.join(log_base_dir,'run_{0:02d}'.format(run_number))
+
+train_summary_writer = tf.summary.FileWriter(log_dir + '/train')
+valid_summary_writer = tf.summary.FileWriter(log_dir + '/valid')
+test_summary_writer = tf.summary.FileWriter(log_dir + '/test')
+model_save_path = log_dir + '/models'
+
+
+
+#############################
+# Setup graph
+#############################
 tf.reset_default_graph()
 X = tf.placeholder(tf.float32, shape=(batch_size, x_in_dim, time_steps))
 
@@ -134,24 +205,22 @@ with tf.variable_scope('loss'):
     total_loss_summ = tf.summary.scalar('total_loss', total_loss)
 
 global_step = tf.Variable(0,name='global_step') 
-epoch_num = tf.Variable(1, name='epoch_num', trainable=False, dtype=tf.int32)
-increment_epoch_num_op = tf.assign(epoch_num, epoch_num+1)
 
-
-learning_rate = tf.train.exponential_decay(starter_learning_rate, epoch_num, num_epochs, decay_rate, staircase=False)
+#learning_rate = tf.train.exponential_decay(initial_learning_rate, epoch_num, num_epochs, decay_rate, staircase=False)
+learning_rate = tf.Variable(learning_rate_1,name='learning_rate')
 train_step = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta_1, beta2=beta_2).minimize(total_loss,global_step=global_step)    
 scalar_summaries = tf.summary.merge([latent_loss_summ, recon_loss_summ, total_loss_summ])
-#image_summaries = tf.summary.merge() 
+#image_summaries = tf.summary.merge()
 
-train_summary_writer = tf.summary.FileWriter('./logs', tf.get_default_graph())
+train_summary_writer.add_graph(tf.get_default_graph())
 
 
-##################################
-# Training
-##################################
+#############################
+# Training/Logging
+#############################
 num_batches = int(num_train_samples/batch_size)
 global_step_op = tf.train.get_global_step()
-
+saver = tf.train.Saver()
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
@@ -160,18 +229,42 @@ with tf.Session() as sess:
         epoch_latent_loss = 0.
         for batch in range(num_batches):
             batch_num = sess.run(global_step_op)
-            _ , loss, scalar_summaries_, x_out_, x_in_,learning_rate_,latent_loss_ = sess.run([train_step, total_loss, scalar_summaries, x_out, x_in,learning_rate, latent_loss],feed_dict=feed_dict(batch_size))
-            train_summary_writer.add_summary(scalar_summaries_, global_step=batch_num)
+            
+            if epoch < num_epochs_to_diff_learn_rate:
+                curr_learning_rate = learning_rate_1
+            else:
+                curr_learning_rate = learning_rate_2
+            
+            #_ , loss, scalar_train_summaries, x_out_, x_in_,learning_rate_,latent_loss_ = \
+            #sess.run([train_step, total_loss, scalar_summaries, x_out, x_in,learning_rate, latent_loss],feed_dict={X: feed_dict(batch_size,'train'), learning_rate: curr_learning_rate})
+            
+            _ , loss, scalar_train_summaries, learning_rate_, latent_loss_ = \
+            sess.run([train_step, total_loss, scalar_summaries, learning_rate, latent_loss],feed_dict={X: feed_dict(batch_size,'train'), learning_rate: curr_learning_rate})
+            
+            # Check for NaN
+            if np.isnan(loss):
+                sys.exit("Loss during training at epoch: {}".format(epoch))
+            
             epoch_loss += loss
             epoch_latent_loss += latent_loss_
             
-            sigma_sq_enc_ = sess.run(sigma_sq_enc, feed_dict=feed_dict(batch_size))
-            #Tracer()()
-            
-            
-            #print('Epoch Loss: {}'.format(loss))
         print('Average loss epoch {0}: {1}'.format(epoch, epoch_loss/num_batches)) 
         print('Average latent loss epoch {0}: {1}'.format(epoch, epoch_latent_loss/num_batches)) 
         print('Learning Rate {}'.format(learning_rate_))
-        sess.run(increment_epoch_num_op)
-
+        
+        # Write train summaries once a epoch
+        scalar_train_summaries = sess.run(scalar_summaries,feed_dict={X: feed_dict(batch_size,'train')})
+        train_summary_writer.add_summary(scalar_train_summaries, global_step=batch_num)
+        
+        # Write validation summaries
+        scalar_valid_summaries = sess.run(scalar_summaries,feed_dict={X: feed_dict(batch_size,'valid')})
+        valid_summary_writer.add_summary(scalar_valid_summaries, global_step=batch_num)
+        
+        # Write test summaries
+        scalar_test_summaries = sess.run(scalar_summaries,feed_dict={X: feed_dict(batch_size,'test')})
+        test_summary_writer.add_summary(scalar_test_summaries, global_step=batch_num)
+        
+        # Save the models
+        if epoch % num_epochs_to_save_model == 0:
+            save_path = saver.save(sess, model_save_path + '/epoch_{}.ckpt'.format(epoch))
+        
